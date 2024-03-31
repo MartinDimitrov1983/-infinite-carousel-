fb97c76d11463eed5817a14826265dff129a155a
Martin Dimitov <mydimi83@gmail.com>
2024-03-31 02:07:33 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { Carousel } from './components';
 import { images } from './utils';
 
 function App() {
-  return <Carousel images={images} height={50} />;
+  return (
+    <div style={{ marginTop: '100px' }}>
+      <Carousel images={images} height={50} />
+    </div>
+  );
 }
 
 export default App;
